73c244bfb2c88d6cea0ba0ca455563c4534bd611
Kawser Ahmed <126769837+kawser-ahmed-byte@users.noreply.github.com>
2024-01-15 23:22:36 +0600

    Update 1.c

--- 1D/1.c
+++ 1D/1.c
@@ -4,11 +4,11 @@ void main()
    int i,n,a[100];
    printf("Input the number: ");
    scanf("%d",&n);
-   for(i=0; i < n; i++)
+   for(i=0; i < n; i++) //for taking user input
       {
 	  scanf("%d", &a[i]);
 	  }
-   for(i=n-1; i>=0; i--)
+   for(i=n-1; i>=0; i--) //for printing result
      {
 	   printf("%d ", a[i]);
 	 }
